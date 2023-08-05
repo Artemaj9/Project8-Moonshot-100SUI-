de7bc1946417c5a6ae409a7b5abd3a4b850b1f70
Artem <moiseev@physics.msu.ru>
2023-08-06 01:35:28 +0300

    Set up all views and  navigation links

---
+++ Project8-Moonshot(100SUI)/AstronautView.swift
@@ -0,0 +1,34 @@
+//
+//  AstronautView.swift
+//  Project8-Moonshot(100SUI)
+//
+//  Created by Artem on 06.08.2023.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["aldrin"]!)
+            .preferredColorScheme(.dark)
+    }
+}
